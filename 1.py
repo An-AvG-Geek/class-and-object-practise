31980100c7e4c88422337320e6fe1b94d1cd12a3
An-AvG-Geek <felixyt2026@gmail.com>
2026-05-10 20:48:57 +0530

    test commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+s1=Student("felix",50)
+s2=Student("jobi",25)
+
+
+print(f"the name of the first student is {s1.name}")
+print(f'the age of the second student is {s2.age}');
+
+
+
+
