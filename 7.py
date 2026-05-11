0373d0870256947a518f32e600b3a59012ba263e
An-AvG-Geek <felixyt2026@gmail.com>
2026-05-11 16:58:55 +0530

    update for hospital management system

--- 7.py
+++ 7.py
@@ -16,8 +16,7 @@ class Doctor:
     def display_doctor_info(self):
         print(f"the doctor is {self.name} and from {self.department}")
 
-    def add_patient(self,patient_name):
-        self.patients.append(patient_name)
+    
     
     def display_patients(self):
         for i in self.patients:
@@ -28,6 +27,29 @@ class Patient:
     def __init__(self,name,phone):
         self.phone=phone
         self.name=name
+        self.appointments=[]
+
+    def book_appointments(self,doctor):
+        self.appointments.append(doctor)
+        doctor.patients.append(self)
+    
+    def show_appointments(self):
+
+        if len(self.appointments)==0:
+            print(f"there are no appointments for {self.name}")
+
+        for i in self.appointments:
+            print(f"patient {self.name} have appoiment with doctor {i.name}")
+    def cancel_appointments(self,doctor):
+
+        if doctor in self.appointments:
+            self.appointments.remove(doctor)
+            doctor.patients.remove(self)
+    
+  
+
+
+
 
     
 p1=Patient("felix",234)
@@ -36,12 +58,16 @@ p2=Patient("jobi",678)
 d1=Doctor("ethan","neuro")
 d2=Doctor("melvin","cardio")
 
-d1.add_patient(p1)
-d1.add_patient(p2)
-d1.display_patients()
 
-#add appoiment list to patient to show list of appointments the patient has
-#add a function in patient to remove his or her appointment. it should also refect to the doctors patients list
+p1.book_appointments(d1)
+p2.book_appointments(d2)
+d1.display_patients()
+p1.show_appointments()
+
+p1.cancel_appointments(d1)
+p1.show_appointments()
+
+
 
     
 
